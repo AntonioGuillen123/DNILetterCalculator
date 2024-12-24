a4ced01df0a7148d5056a9178d4be56a1a34fe3a
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2024-12-24 14:16:18 +0100

    feat: Create Dni Letter Factory

---
+++ database/factories/DniLetterFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\DniLetter>
+ */
+class DniLetterFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $letters = [
+            'T',
+            'R',
+            'W',
+            'A',
+            'G',
+            'M',
+            'Y',
+            'F',
+            'P',
+            'D',
+            'X',
+            'B',
+            'N',
+            'J',
+            'Z',
+            'S',
+            'Q',
+            'V',
+            'H',
+            'L',
+            'C',
+            'K',
+            'E'
+        ];
+
+        return [
+            'letter' => $this->faker->randomElement($letters)
+        ];
+    }
+}

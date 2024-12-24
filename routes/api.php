8d8c5711de989c33806fdb1e7b42f0ca7499065f
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2024-12-24 14:05:03 +0100

    feat: Create Dni Calculator Route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\DniLetterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/dniCalculator', [DniLetterController::class, 'index'])->name('index');
